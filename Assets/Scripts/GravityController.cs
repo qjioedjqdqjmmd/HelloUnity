adb9b5fecead2ca7dfe8219b8ff0d5ddb7e6a297
qjioedjqdqjmmd <34190387+qjioedjqdqjmmd@users.noreply.github.com>
2018-01-25 02:52:12 +0900

    충돌 추가

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -18,19 +18,30 @@ public class GravityController : MonoBehaviour {
         //중력 벡터 초기화
         Vector3 vector = new Vector3();
 
-        //키 입력을 검출하는 벡터 생성
-        vector.x = Input.GetAxis("Horizontal");
-        vector.z = Input.GetAxis("Vertical");
-
-        //높이 방향의 판정은 z키로 한다.
-        if (Input.GetKey("z"))
+        if (Application.isEditor)
         {
-            vector.y = 1.0f;
+            //키 입력을 검출하는 벡터 생성
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
+
+            //높이 방향의 판정은 z키로 한다.
+            if (Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
         }
         else
         {
-            vector.y = -1.0f;
+            //가속도 센서의 입력을 유니티 공간의 축에 매핑한다.
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = Input.acceleration.z;
         }
+        
 
         //씬의 중력을 입력 벡터의 방향에 맞추어 변화 시킨다.
         Physics.gravity = Gravity * vector.normalized * gravityScale;
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+    //어떤 공을 빨아들일지를 태그로 지정
+    public string activeTag;
+
+    private void OnTriggerStay(Collider other)
+    {
+        //콜라이더에 접촉하고 있는 오브젝트의 Rigidbody 컴퍼넌트 취득
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        //공이 어떤 방향에 있는지를 계산
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        //태그에 따라 공에 힘을 더한다.
+        if(other.gameObject.tag == activeTag)
+        {
+            //중심 지점에서 공을 멈추기 위해 속도를 감소 시킨다.
+            r.velocity *= 0.9f;
+
+            r.AddForce(direction * r.mass * 20.0f);
+        }
+        else
+        {
+            r.AddForce(-direction * r.mass * 80.0f);
+        }
+    }
+}
